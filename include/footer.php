24e4b80359b588e737c937824d0380a001299c89
Aryan Saxena <aryansaxena136@gmail.com>
2025-08-10 11:57:20 +0530

    intendation

--- include/footer.php
+++ include/footer.php
@@ -1,25 +1,25 @@
   <!--footer-->
   <div class="footer ">
-    <h5>Be sure to contact with us</h5>
-    <div class="social-icons">
-      <a href="https://facebook.com" target="_blank">
-        <i class="fa fa-facebook-square"></i>
-      </a>
-      <a href="https://twitter.com" target="_blank">
-        <i class="fa fa-twitter"></i>
-      </a>
-      <a href="https://youtube.com" target="_blank">
-        <i class="fa fa-youtube-play"></i>
-      </a>
-      <a href="https://instagram.com" target="_blank">
-        <i class="fa fa-instagram"></i>
-      </a>
-      <a href="https://linkedin.com" target="_blank">
-        <i class="fa fa-linkedin"></i>
-      </a>
-    </div>
+      <h5>Be sure to contact with us</h5>
+      <div class="social-icons">
+          <a href="https://facebook.com" target="_blank">
+              <i class="fa fa-facebook-square"></i>
+          </a>
+          <a href="https://twitter.com" target="_blank">
+              <i class="fa fa-twitter"></i>
+          </a>
+          <a href="https://youtube.com" target="_blank">
+              <i class="fa fa-youtube-play"></i>
+          </a>
+          <a href="https://instagram.com" target="_blank">
+              <i class="fa fa-instagram"></i>
+          </a>
+          <a href="https://linkedin.com" target="_blank">
+              <i class="fa fa-linkedin"></i>
+          </a>
+      </div>
   </div>
   </div>
-</body>
+  </body>
 
-</html>
+  </html>
